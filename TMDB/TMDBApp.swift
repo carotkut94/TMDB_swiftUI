1a2dace45c1e23653e4801981aafd9c446efedbe
carotkut94 <carotkut12@gmail.com>
2021-02-04 01:12:37 +0530

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Death Code on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
